3bd07aab46d1188370deb8ae280b03b3dbe6e126
Nik <semushin.nik@inbox.ru>
2021-12-07 19:40:39 +0500

    Abstract class and interface

--- src/Main.kt
+++ src/Main.kt
@@ -6,10 +6,18 @@ fun main() {
         item.roam()
         item.eat()
     }
-    
+
     val vet = Vet()
     val wolf = Wolf()
     val hippo = Hippo()
     vet.giveShot(wolf)
     vet.giveShot(hippo)
+
+    val roamable = arrayOf(Hippo(), Wolf(), Vehicle())
+    for (item in roamable) {
+        item.roam()
+        if (item is Animal)
+            item.eat()
+        println("")
+    }
 }
--- src/Vehicle.kt
+++ src/Vehicle.kt
@@ -1,2 +1,5 @@
-class Vehicle {
+class Vehicle : Roamable {
+    override fun roam() {
+        println("The Vehicle is roaming")
+    }
 }
